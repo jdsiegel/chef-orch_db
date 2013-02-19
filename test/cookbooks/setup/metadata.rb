dbd312834b91f40beebd202b6f7df35d546c38bb
Jeff Siegel <jdsiegel@gmail.com>
2013-02-18 20:26:55 -0700

    remove cookbook deps from setup cookbook

--- test/cookbooks/setup/metadata.rb
+++ test/cookbooks/setup/metadata.rb
@@ -4,6 +4,3 @@ license          "All rights reserved"
 description      "Installs/Configures setup"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
-
-depends          "postgresql"
-depends          "mysql"
